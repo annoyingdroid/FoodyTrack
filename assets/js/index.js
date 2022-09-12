6c0b825022dbf2c143c21c58579cc5ac02b6a647
Mpergolatti <Mpergolatti@gmail.com>
2022-09-12 19:07:59 -0400

    Styling

--- assets/js/index.js
+++ assets/js/index.js
@@ -58,7 +58,6 @@ function loadFilters() {
 // declare get recipes function
 function getRecipes(qParam){
     var apiUrl = "https://api.edamam.com/api/recipes/v2?type=public&app_id=b08dc2fb&app_key=783268a2de0b8c46cf30721531506847&q=" + qParam;
-
     // make request to the url
     fetch(apiUrl).then(function (response) {
         if (response.ok) {
